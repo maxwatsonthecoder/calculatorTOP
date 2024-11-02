394aa6bb75e5ce3be4783a6612da6386e8ab755a
maxwatsonthecoder <140012390+maxwatsonthecoder@users.noreply.github.com>
2024-11-02 01:03:24 -0500

    Add basic CSS layout

--- script.js
+++ script.js
@@ -6,27 +6,17 @@ const add = function(a, b) {
     return a + b
 };
 
-console.log(add(1, 2));
-
 const subtract = function(a, b) {
     return a - b
 };
-
-console.log(subtract(1, 2))
 
 const multiply = function(a, b) {
     return a * b
 };
 
-console.log(multiply(1, 2))
-
-
 const divide = function(a, b) {
     return a / b
 };
-
-console.log(divide(1, 2))
-
 
 const operate = function() {
 
