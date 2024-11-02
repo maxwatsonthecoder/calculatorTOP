4128a9879fe6ce017e468850a40b5e6d720ead50
maxwatsonthecoder <140012390+maxwatsonthecoder@users.noreply.github.com>
2024-11-01 23:59:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let num1 = "";
+let num2 = "";
+const operator = ["+", "-" , "*", "/"];
+
+const add = function(a, b) {
+    return a + b
+};
+
+console.log(add(1, 2));
+
+const subtract = function(a, b) {
+    return a - b
+};
+
+console.log(subtract(1, 2))
+
+const multiply = function(a, b) {
+    return a * b
+};
+
+console.log(multiply(1, 2))
+
+
+const divide = function(a, b) {
+    return a / b
+};
+
+console.log(divide(1, 2))
+
+
+const operate = function() {
+
+}
