c5dd98aa0360ceb55955489939e8fe3adf4c8687
maxwatsonthecoder <140012390+maxwatsonthecoder@users.noreply.github.com>
2024-11-03 00:33:45 -0500

    Attempt operate functions. Fail. Try again tomorrow.

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 let num1 = "";
 let num2 = "";
-const operator = ["+", "-" , "*", "/"];
+let operator = null;
+let isNum2 = false;
+let result = "";
 
 const numbersBtns = document.getElementById("numbers");
+const operatorsBtns = document.getElementById("operators");
+const equalsBtn = document.getElementById("equals-btn");
 
+//numbersBtns.addEventListener("click", initiate);
 
-//in progress main function
-numbersBtns.addEventListener("click", (event) => {
-    if (event.target.tagName === "BUTTON") {
-        num1 += event.target.textContent;
-        console.log(`${num1}`);
-    }
-})
+function chosenNum1() {
+    numbersBtns.addEventListener("click", (event) => {
+        if (event.target.tagName === "BUTTON") {
+            isNum2 = false;
+            num1 += event.target.textContent;
+            console.log(`${num1}`);
+        }
+    });
+};
 
+function chosenNum2() {
+    numbersBtns.addEventListener("click", (event) => {
+        if (event.target.tagName === "BUTTON") {
+            isNum2 = true;
+            num2 += event.target.textContent;
+            console.log(`${num2}`);
+        }
+    });
+};
+
+const chosenOperator = operatorsBtns.addEventListener("click", (event) => {
+        if (event.target.tagName === "BUTTON") {
+            operator = event.target.textContent;
+            console.log(`${operator}`);
+        }
+    });
 
 function add(a, b) {
     return a + b
@@ -47,7 +70,53 @@ function operate() {
     }
 }
 
+
+
+//work in progress main function
 function initiate() {
-    console.log("numbers div clicked!")
+
+    const chosenOperator = operatorsBtns.addEventListener("click", (event) => {
+        if (event.target.tagName === "BUTTON") {
+            operator = event.target.textContent;
+            console.log(`${operator}`);
+        }
+    });
+
+    function chosenNum() {
+        numbersBtns.addEventListener("click", (event) => {
+            if (event.target.tagName === "BUTTON") {
+                isNum2 = false;
+                num1 += event.target.textContent;
+                console.log(`${num1}`);
+            }
+        });
+    };
+
+    function chosenNum2() {
+        numbersBtns.addEventListener("click", (event) => {
+            if (event.target.tagName === "BUTTON") {
+                isNum2 = true;
+                num2 += event.target.textContent;
+                console.log(`${num2}`);
+            }
+        });
+    };
+
+    function operate() {
+        switch (operator) {
+            case "+":
+                return add(num1, num2);
+                break;
+            case "-":
+                return subtract(num1, num2);
+                break;
+            case "*":
+                return multiply(num1, num2);
+                break;
+            case "/":
+                return divide(num1, num2);
+                break;
+        }
+    }
 } 
 
